31a7c45a9e22512e6d40d1a8f4a68abfdb42b16f
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-12-03 21:37:43 +0200

    Add project files.

---
+++ MvcMovie/Controllers/Test2222Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class Test2222Controller : Controller
+    {
+        // GET: Test2222
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
